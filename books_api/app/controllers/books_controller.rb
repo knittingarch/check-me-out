eafe4c1055031f771eae0c3fae0e6b17f7a8d52f
Sarah Dawson <knittingarch@github.com>
2025-08-30 13:07:33 -0500

    Add controller

---
+++ books_api/app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: %i[ show update destroy ]
+
+  # GET /books
+  # GET /books.json
+  def index
+    @books = Book.all
+
+    render json: @books
+  end
+
+  # GET /books/1
+  # GET /books/1.json
+  def show
+    render json: @book
+  end
+
+  # POST /books
+  # POST /books.json
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /books/1
+  # PATCH/PUT /books/1.json
+  def update
+    if @book.update(book_params)
+      render json: @book, status: :ok
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/1
+  # DELETE /books/1.json
+  def destroy
+    @book.destroy
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :isbn, :published_date, :status, :borrowed_until)
+    end
+end
