5126e5830bf40dfa58c110cb0076f213e1b0ea1a
Sarah Dawson <knittingarch@github.com>
2025-08-31 21:02:02 -0500

    Add Author association

--- books_api/db/schema.rb
+++ books_api/db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_30_174040) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_01_014306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "author_id", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
